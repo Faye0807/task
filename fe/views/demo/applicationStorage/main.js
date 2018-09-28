a11bc1176a68919d1dc4836cd3b171ac427c853f
littleFaye <1748711164@qq.com>
2018-09-28 10:58:15 +0800

    add test demo

---
+++ fe/views/demo/applicationStorage/main.js
@@ -0,0 +1,8 @@
+console.log(document.cookie);
+// document.cookie = 'name=hengheng';
+document.cookie = 'ages=18;domain=.maimaiche.com;';
+// document.cookie = 'domain=maimaiche.com';
+console.log(document.cookie);
+console.log(sessionStorage);
+sessionStorage.sessionStoragedata = 'oneData';
+console.log(sessionStorage);
---
+++ fe/views/demo/jsNumTransform/main.js
@@ -0,0 +1,7 @@
+const opena = ()=>{
+  window.open('https://www.baidu.com','ahahahha','width=320;height=480')
+};
+
+// function open(){
+//   window.open('https://www.baidu.com','ahahahha','width=320;height="480"');
+// }
